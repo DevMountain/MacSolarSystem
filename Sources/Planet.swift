f039be259ed5b943bb592bc54d0484548e009664
Andrew Madsen <andrew@openreelsoftware.com>
2016-12-13 11:08:03 -0700

    Add model and model controller.

---
+++ Sources/Planet.swift
@@ -0,0 +1,57 @@
+//
+//  Planet.swift
+//  SolarSystem
+//
+//  Created by Caleb Hicks on 9/28/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Planet {
+  
+  init(name: String, diameter: Int, dayLength: Float, millionKMsFromSun: Float) {
+    
+    self.name = name
+    self.imageName = name.lowercased()
+    self.diameter = diameter
+    self.dayLength = dayLength
+    self.millionKMsFromSun = millionKMsFromSun
+  }
+  
+  let name: String
+  let imageName: String
+  let diameter: Int
+  let dayLength: Float
+  let millionKMsFromSun: Float
+}
+
+extension Planet {
+  
+  static var NameKey: String { return "name" }
+  static var DiameterKey: String { return "diameter" }
+  static var DayLengthKey: String { return "dayLength" }
+  static var DistanceKey: String { return "millionKMsFromSun" }
+  
+  convenience init?(dictionary: [String : Any]) {
+    guard let name = dictionary[Planet.NameKey] as? String,
+      let diameter = dictionary[Planet.DiameterKey] as? Int,
+      let dayLength = dictionary[Planet.DayLengthKey] as? Float,
+      let distance = dictionary[Planet.DistanceKey] as? Float else { return nil }
+    
+    self.init(name: name, diameter: diameter, dayLength: dayLength, millionKMsFromSun: distance)
+  }
+  
+  var dictionaryRepresentation: [String : Any] {
+    return [Planet.NameKey : name,
+            Planet.DiameterKey : diameter,
+            Planet.DayLengthKey : dayLength,
+            Planet.DistanceKey : millionKMsFromSun]
+  }
+}
+
+extension Planet: CustomStringConvertible {
+  var description: String {
+    return "\(name) - Diameter: \(diameter) Day Length: \(dayLength) Distance: \(millionKMsFromSun) million km"
+  }
+}
---
+++ Sources/PlanetController.swift
@@ -0,0 +1,34 @@
+//
+//  PlanetController.swift
+//  SolarSystem
+//
+//  Created by Caleb Hicks on 9/28/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class PlanetController {
+  
+  static let sharedController = PlanetController()
+  
+  private func loadPlanets() throws -> [Planet] {
+    guard let planetFileURL = Bundle.main.url(forResource: "planets", withExtension: "json") else {
+      throw NSError(domain: "com.devmountain.SolarSystem.ErrorDomain", code: -1, userInfo: nil)
+    }
+    let data = try Data(contentsOf: planetFileURL)
+    guard let planetDicts = try JSONSerialization.jsonObject(with: data, options: []) as? [[String : Any]] else {
+      throw NSError(domain: "com.devmountain.SolarSystem.ErrorDomain", code: -2, userInfo: nil)
+    }
+    return planetDicts.flatMap { Planet(dictionary: $0) }
+  }
+  
+  lazy var planets: [Planet] = {
+    do {
+      return try self.loadPlanets()
+    } catch {
+      NSLog("Error loading planets from JSON: \(error)")
+      return []
+    }
+  }()
+}
