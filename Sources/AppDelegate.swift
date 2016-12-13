a1a6f4e7faf2d1cdc2eee7b84264ef21529bd4c4
Andrew Madsen <andrew@openreelsoftware.com>
2016-12-13 10:36:58 -0700

    Initial commit.

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Solar System
+//
+//  Created by Andrew Madsen on 12/13/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Sources/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Solar System
+//
+//  Created by Andrew Madsen on 12/13/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
